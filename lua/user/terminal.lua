cb7d33f54075f0e4077d5c3a2de81f72311e109e
Simax <simaxme@gmail.com>
2022-07-24 16:34:47 +0200

    update

--- lua/user/terminal.lua
+++ lua/user/terminal.lua
@@ -1,9 +1,98 @@
-vim.api.nvim_set_keymap("n", "<C-t>", [[
-    :Gcd<CR>
-    :belowright split<CR>
-    :term<CR>
-    :resize 8<CR>
-    i
-]], {noremap = true})
+local terminal = {}
+local utils = require('user.utils')
 
+-- CONSTANTS
+local HORIZONTAL_TERMINAL_SIZE_FULL = 15
+local HORIZONTAL_TERMINAL_SIZE_MIN = 4
+local VERTICAL_TERMINAL_SIZE_MIN = 25
+local VERTICAL_TERMINAL_SIZE_FULL = 45
+-- ----
+
+function getBuffName()
+    return vim.fn.bufname()
+end
+function terminal.focusTerminalInput()
+    -- focus current window after switching to terminal
+    vim.cmd [[normal! i]]
+end
+function createTerminal(command)
+    if command ~= nil then
+        vim.cmd ([[term ]] .. command)
+    else
+        vim.cmd [[term]]
+    end
+    terminal.focusTerminalInput()
+end
+function terminal.createVerticalTerminal(command)
+    vim.cmd [[belowright vsplit]]
+    createTerminal(command)
+    vim.cmd ([[vertical resize ]] .. VERTICAL_TERMINAL_SIZE_FULL)
+    vim.cmd ([[file VE_]] .. getBuffName())
+end
+function terminal.createHorizontalTerminal(command)
+    vim.cmd [[belowright split]]
+    createTerminal(command)
+    vim.cmd ([[resize ]] .. HORIZONTAL_TERMINAL_SIZE_FULL)
+    vim.cmd ([[file HO_]] .. getBuffName())
+end
+
+function terminal.getCurrentBufferType()
+    local bufid = vim.fn.bufnr()
+    return vim.fn.getbufvar(bufid, '&buftype', 'ERROR')
+end
+function terminal.isCurrentBufferTerminal()
+    return terminal.getCurrentBufferType() == "terminal"
+end
+
+function terminal.beforeBufferLeave()
+    if terminal.isCurrentBufferTerminal() then
+        if utils.stringStartsWith(getBuffName(), "HO") then
+            vim.cmd ([[resize ]] .. HORIZONTAL_TERMINAL_SIZE_MIN)
+        elseif utils.stringStartsWith(getBuffName(), "VE") then 
+            vim.cmd ([[vertical resize ]] .. VERTICAL_TERMINAL_SIZE_MIN)
+        end
+    end
+end
+function terminal.afterBufferLeave()
+    if terminal.isCurrentBufferTerminal() then
+        if utils.stringStartsWith(getBuffName(), "HO") then
+            vim.cmd ([[resize ]] .. HORIZONTAL_TERMINAL_SIZE_FULL)
+        elseif utils.stringStartsWith(getBuffName(), "VE") then 
+            vim.cmd ([[vertical resize ]] .. VERTICAL_TERMINAL_SIZE_FULL)
+        end
+        terminal.focusTerminalInput()
+    end
+end
+
+vim.cmd [[
+autocmd BufLeave * lua require('user.terminal').beforeBufferLeave()
+autocmd BufEnter * lua require('user.terminal').afterBufferLeave() 
+]]
+
+vim.api.nvim_create_user_command(
+    'VTerminal',
+    function (args)
+        terminal.createVerticalTerminal(nil)
+    end,
+    {nargs = '*'}
+)
+vim.api.nvim_create_user_command(
+    'Terminal',
+    function (args)
+        terminal.createHorizontalTerminal(nil)
+    end,
+    {nargs = '*'}
+)
+vim.api.nvim_create_user_command(
+    'HTerminal',
+    function (args)
+        terminal.createHorizontalTerminal(nil)
+    end,
+    {nargs = '*'}
+)
+
+vim.api.nvim_set_keymap("n", "<C-t>", ":lua require('user.terminal').createHorizontalTerminal()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
+vim.api.nvim_set_keymap("t", "<C-w>", "<C-\\><C-n><C-w>", {noremap = true})
+
+return terminal
--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -1,6 +1,6 @@
-local x = {}
+local utils = {}
 
-function x.split(pString, pPattern)
+function utils.split(pString, pPattern)
    local Table = {}  -- NOTE: use {n = 0} in Lua-5.0
    local fpat = "(.-)" .. pPattern
    local last_end = 1
@@ -19,8 +19,32 @@ function x.split(pString, pPattern)
    return Table
 end
 
-function x.echo(str)
+function utils.echo(str)
     vim.cmd([[echo ']] .. str .. [[']])
 end
+function utils.error(str)
+    vim.cmd([[echoerr ']] .. str .. [[']])
+end
 
-return x
+function utils.getCWD()
+    return vim.api.nvim_eval("getcwd()")
+end
+
+function utils.fileExists(path)
+    local f = io.open(path, "r")
+    if f~=nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+
+function utils.stringStartsWith(String, Start)
+    return string.sub(String, 1, string.len(Start)) == Start
+end
+function utils.stringEndsWithWith(String, End)
+    return string.sub(String, string.len(String) - string.len(End) + 1, string.len(String)) == End
+end
+
+return utils
