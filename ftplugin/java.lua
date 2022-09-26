3673c7f8264e3290ae60e6c76987c65cc5b489c6
Simax <simaxme@gmail.com>
2022-09-26 16:40:40 +0200

    update

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,10 +1,13 @@
 local ssession = require('user.ssession.lib')
+local java = require('user.java')
 
+local version = java.findJavaVersion()
 
 require('jdtls').start_or_attach{
   cmd = {
     '/home/simon/.config/nvim/scripts/java-lsp.sh',
-    ssession.getCurrentCWDId()
+    ssession.getCurrentCWDId(),
+    version
   }
 }
 
