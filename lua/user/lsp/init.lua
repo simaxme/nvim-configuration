5f5b4eb10875f1a94ea281551a556f303bf48ea0
simaxme <contact@simax.me>
2023-03-03 21:34:02 +0100

    update

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -26,8 +26,7 @@ local function onAttach(client, bufnr)
     end
 end
 
-local capabilities  = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 
 require('lspconfig').tsserver.setup { on_attach = onAttach }
