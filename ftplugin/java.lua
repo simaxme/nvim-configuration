ec0ee4dbd722855bcfb671ba562c60f3587c2f2a
simaxme <contact@simax.me>
2023-10-28 13:29:47 +0200

    updated nvim config to make jdtls working correctly with macos

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,11 +3,39 @@ local ssession = require('user.ssession.lib')
 
 -- local version = java.findJavaVersion()
 
+
+local home = os.getenv("HOME")
+
 require('jdtls').start_or_attach{
   cmd = {
-    '/home/simon/.config/nvim/scripts/java-lsp.sh',
+    "bash",
+    home .. '/.config/nvim/scripts/java-lsp.bash',
     ssession.getCurrentCWDId(),
     "java"
+  },
+
+  settings = {
+    java = {
+      configuration = {
+        -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+        -- And search for `interface RuntimeOption`
+        -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+        runtimes = {
+          {
+            name = "JavaSE-17",
+            path = "/Users/simon/.config/nvim/jdk/jdk-17.0.2.jdk/Contents/Home/",
+          },
+          {
+            name = "JavaSE-18",
+            path = "/Users/simon/.config/nvim/jdk/jdk-18.jdk/Contents/Home/",
+          },
+          -- {
+          --   name = "JavaSE-21",
+          --   path = "/Users/simon/.config/nvim/jdk/jdk-21.jdk/Contents/Home/",
+          -- },
+        }
+      }
+    }
   }
 }
 
