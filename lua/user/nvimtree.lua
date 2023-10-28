7a0b823fc6221985f69559f95a44b8719cf39473
Simax <simaxme@gmail.com>
2023-10-28 13:29:47 +0200

    updated nvim config to make jdtls working correctly with macos

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -51,6 +51,11 @@ require('nvim-tree').setup({
         --         { key = "fg", action = "telescope_grep", action_cb = function() vim.cmd("Telescope live_grep") end }
         --     }
         -- }
+    },
+
+    git = {
+        enable = true,
+        ignore = false
     }
 })
 
@@ -78,5 +83,26 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 vim.cmd [[
     autocmd VimEnter * hi NvimTreeVertSplit guibg=NONE guifg=background 
 ]]
+
+-- bd fix
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    local api = require('nvim-tree.api')
+
+    -- Only 1 window with nvim-tree left: we probably closed a file buffer
+    if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+      -- Required to let the close event complete. An error is thrown without this.
+      vim.defer_fn(function()
+        -- close nvim-tree: will go to the last hidden buffer used before closing
+        api.tree.toggle({find_file = true, focus = true})
+        -- re-open nivm-tree
+        api.tree.toggle({find_file = true, focus = true})
+        -- nvim-tree is still the active window. Go to the previous window.
+        vim.cmd("wincmd p")
+      end, 0)
+    end
+  end
+})
 
 return M
