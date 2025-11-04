b6857bd2a55d16e266279ed05eed684bf2d2b243
simaxme <contact@simax.me>
2025-11-04 21:18:03 +0100

    Removed mini.git and mini.animate

--- src/lua/user/plugins/mini/init.lua
+++ src/lua/user/plugins/mini/init.lua
@@ -1,10 +1,8 @@
 local function setup()
   require("user.plugins.mini.ai")
   require("user.plugins.mini.cursorword")
-  require("user.plugins.mini.git")
   require("user.plugins.mini.indentscope")
   require("user.plugins.mini.surround")
-  -- require("user.plugins.mini.animate")
 end
 
 return {
