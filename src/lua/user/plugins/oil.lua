615b522703feaca2563b8abdfb4a05f4e4d934c2
simaxme <contact@simax.me>
2025-12-14 16:56:47 +0100

    Added simaxme/oil-angular-schematics.nvim to plugins

--- src/lua/user/plugins/oil.lua
+++ src/lua/user/plugins/oil.lua
@@ -13,9 +13,39 @@ return {
       },
       view_options = {
         show_hidden = true
+      },
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        ["<Leader>ng"] = {
+          mode = "n",
+          callback = function()
+            require("oil-angular-schematics").create_angular_schematic()
+          end
+        }
       }
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  {
+    "simaxme/oil-angular-schematics.nvim",
+
+    version = "*" -- for stable releases
   }
 }
