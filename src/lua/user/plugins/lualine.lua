d9ef7c94d20dd0d6ca74d1aceaf0f46ee4b9f1ed
simaxme <contact@simax.me>
2025-12-14 14:29:36 +0100

    Switched to 'opts' field in plugins where possible

--- src/lua/user/plugins/lualine.lua
+++ src/lua/user/plugins/lualine.lua
@@ -7,20 +7,18 @@ return {
     'nvim-lualine/lualine.nvim',
     commit = "47f91c416daef12db467145e16bed5bbfe00add8",
 
-    config = function()
-      require('lualine').setup {
-        options = {
-          refresh = {
-            statusline = 5000
-          },
-          theme = "monokai-pro"
+    opts = {
+      options = {
+        refresh = {
+          statusline = 5000
         },
-        sections = {
-          lualine_x = { get_current_tabs, 'encoding', 'fileformat', 'filetype' },
-          lualine_y = { "lsp_status" },
-          lualine_z = { 'progress', 'location' }
-        }
+        theme = "monokai-pro"
+      },
+      sections = {
+        lualine_x = { get_current_tabs, 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { "lsp_status" },
+        lualine_z = { 'progress', 'location' }
       }
-    end
+    }
   }
 }
--- src/lua/user/plugins/neogen.lua
+++ src/lua/user/plugins/neogen.lua
@@ -1,12 +1,8 @@
-function setup()
-  require("neogen").setup()
-end
-
 return {
   {
     "danymat/neogen",
     commit = "d7f9461727751fb07f82011051338a9aba07581d",
-    config = setup,
+    opts = {}
     -- Uncomment next line if you want to follow only stable versions
     -- version = "*"
   }
