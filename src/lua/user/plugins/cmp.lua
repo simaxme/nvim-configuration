1a3a6faf470eb380024553c5863aa92ef19ebabf
simaxme <contact@simax.me>
2026-02-22 20:50:23 +0100

    Removed copilot as completion source from cmp

--- src/lua/user/plugins/cmp.lua
+++ src/lua/user/plugins/cmp.lua
@@ -13,8 +13,7 @@ local function setup()
     sources = cmp.config.sources({
       { name = "luasnip" },
       { name = "nvim_lsp" },
-      { name = "nvim_lsp_signature_help" },
-      { name = "copilot" }
+      { name = "nvim_lsp_signature_help" }
     }),
 
     mapping = cmp.mapping.preset.insert({
